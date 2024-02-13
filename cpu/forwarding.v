48288e9f21ddf6e3ce46864c42129ac3be279b9d
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2024-02-14 08:46:11 +0900

    fixed bug related wb in dc miss

--- cpu/forwarding.v
+++ cpu/forwarding.v
@@ -50,14 +50,14 @@ wire rd_adr_ma_not0 = |rd_adr_ma;
 wire rd_adr_wb_not0 = |rd_adr_wb;
 wire hit_rs1_ldidex = rd_adr_ex_not0 & (inst_rs1_id == rd_adr_ex) & inst_rs1_valid & wbk_rd_reg_ex & cmd_ld_ex;
 wire hit_rs1_idex = rd_adr_ex_not0 & (inst_rs1_id == rd_adr_ex) & inst_rs1_valid & wbk_rd_reg_ex & ~cmd_ld_ex & ~hit_rs1_ldidex_dly;
-wire hit_rs1_idma = rd_adr_ma_not0 & (inst_rs1_id == rd_adr_ma) & inst_rs1_valid & wbk_rd_reg_ma;
-wire hit_rs1_idwb = rd_adr_wb_not0 & (inst_rs1_id == rd_adr_wb) & inst_rs1_valid & wbk_rd_reg_wb;
+wire hit_rs1_idma = (rd_adr_ma_not0 & (inst_rs1_id == rd_adr_ma) & inst_rs1_valid & wbk_rd_reg_ma) | stall_fin2;
+wire hit_rs1_idwb = rd_adr_wb_not0 & (inst_rs1_id == rd_adr_wb) & inst_rs1_valid & wbk_rd_reg_wb  & ~stall_fin2;
 wire nohit_rs1 = ~( hit_rs1_idex | hit_rs1_idma | hit_rs1_idwb);
 
 wire hit_rs2_ldidex = rd_adr_ex_not0 & (inst_rs2_id == rd_adr_ex) & inst_rs2_valid & wbk_rd_reg_ex & cmd_ld_ex;
 wire hit_rs2_idex = rd_adr_ex_not0 & (inst_rs2_id == rd_adr_ex) & inst_rs2_valid & wbk_rd_reg_ex & ~cmd_ld_ex & ~hit_rs2_ldidex_dly;
-wire hit_rs2_idma = rd_adr_ma_not0 & (inst_rs2_id == rd_adr_ma) & inst_rs2_valid & wbk_rd_reg_ma;
-wire hit_rs2_idwb = rd_adr_wb_not0 & (inst_rs2_id == rd_adr_wb) & inst_rs2_valid & wbk_rd_reg_wb;
+wire hit_rs2_idma = (rd_adr_ma_not0 & (inst_rs2_id == rd_adr_ma) & inst_rs2_valid & wbk_rd_reg_ma) | stall_fin2;
+wire hit_rs2_idwb = rd_adr_wb_not0 & (inst_rs2_id == rd_adr_wb) & inst_rs2_valid & wbk_rd_reg_wb & ~stall_fin2;
 wire nohit_rs2 = ~( hit_rs2_idex | hit_rs2_idma | hit_rs2_idwb);
 
 // for stall 1 cycle
@@ -90,7 +90,8 @@ always @ (posedge clk or negedge rst_n) begin
 		hit_rs1_ldidex_dly <= 1'b0;
 		hit_rs2_ldidex_dly <= 1'b0;
 	end
-	else if (~stall) begin
+	//else if (~stall) begin
+	else begin
 		hit_rs1_idex_ex <= hit_rs1_idex;
 		hit_rs1_idma_ex <= hit_rs1_idma;
 		hit_rs1_idwb_ex <= hit_rs1_idwb;
